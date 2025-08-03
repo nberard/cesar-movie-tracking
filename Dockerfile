5fdc9db0eb3c237818783a5b33b75049315ead9f
clearmind <touplitoui.pro@gmail.com>
2025-08-03 16:16:23 +0200

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,6 @@ FROM python:latest
 COPY warmup.py /app/
 COPY input-formatted.json /app/
 COPY requirements.txt /app/
+RUN mkdir /app/cache
 RUN pip install -r /app/requirements.txt
 RUN chmod +x /app/warmup.py
--- warmup.py
+++ warmup.py
@@ -1,6 +1,14 @@
+import urllib
 import json
 
+DATA_IDF_PREFIX = "https://data.iledefrance.fr/api/datasets/1.0/films-soutenus-par-la-region-et-recompenses-aux-cesar/images/"
+
+url_opener = urllib.URLopener()
 with open('/app/input-formatted.json', 'r') as f:
     data = json.load(f)
     for movie in data:
-        print(movie['picture_url'])
+        if DATA_IDF_PREFIX in movie['picture_url']:
+            print(movie['picture_url'])
+            filename =  "/app/cache/" + movie['picture_url'].split("/")[-1] + ".jpg"
+            url_opener.retrieve(movie['picture_url'], filename)
+            print(filename)
